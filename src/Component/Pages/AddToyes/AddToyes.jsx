1b47e21412378cfcce33e3fef3729e6cbf3393ac
mdmorshedsagar <mdmosahidsagar5304257@gmail.com>
2023-05-19 04:27:19 -1200

    addToy-layout

--- src/Component/Pages/AddToyes/AddToyes.jsx
+++ src/Component/Pages/AddToyes/AddToyes.jsx
@@ -1,102 +1,129 @@
+// import Swal from "sweetalert2";
 
+const handleAddCoffee = (event) => {
+  event.preventDefault();
+
+  const form = event.target;
+
+  const name = form.name.value;
+  const picture = form.picture.value;
+  const seller = form.seller.value;
+  const email = form.email.value;
+  const category = form.category.value;
+  const price = form.price.value;
+  const rating = form.rating.value;
+  const quantity = form.quantity.value;
+    const description = form.description.value;
+  const sellers={
+    name,picture,seller,email,category,price,rating,quantity,description
+  }
+
+  
+
+
+
+console.log(sellers)
+ 
+  
+
+};
 
 const AddToyes = () => {
-    return (
-        <div className="bg-indigo-200 p-16">
-        <h2 className="text-4xl font-bold text-center">Add a Toy</h2>
-        <form >
-           
-            <div className="md:flex mb-8">
-                <div className="form-control md:w-1/2">
-                    <label className="label">
-                        <span className="label-text">Toy photo</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="photo" placeholder="Picture URL of the toy" className="input input-bordered w-full" />
-                    </label>
+  return (
+    <div className="bg-[#F4F3F0] p-24">
+            <h2 className="text-4xl font-bold text-center py-4">Add a Toy</h2>
+            <form onSubmit={handleAddCoffee}>
+               
+                <div className="md:flex mb-8 gap-6">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Picture URL of the toy</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="picture" placeholder="Toy picture" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="name" placeholder="Inter toy name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
                 </div>
-                <div className="form-control md:w-1/2 ml-4">
-                    <label className="label">
-                        <span className="label-text">Name</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="name" placeholder="Inter toy name" className="input input-bordered w-full" />
-                    </label>
+                <div className="md:flex mb-8 gap-6">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Seller name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="seller" placeholder="Seller name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ">
+                        <label className="label">
+                            <span className="label-text">Seller email</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="email" name="email" placeholder="Inter toy name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
                 </div>
-            </div>
-            {/* form supplier row */}
-            <div className="md:flex mb-8">
-                <div className="form-control md:w-1/2">
-                    <label className="label">
-                        <span className="label-text">Seller Name</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="sellerName" placeholder="Inter seller name" className="input input-bordered w-full" />
-                    </label>
+                <div className="md:flex mb-8 gap-6">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Sub-category</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="category" placeholder="Sub-category" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="price" placeholder="Inter toy price" className="input input-bordered w-full" />
+                        </label>
+                    </div>
                 </div>
-                <div className="form-control md:w-1/2 ml-4">
-                    <label className="label">
-                        <span className="label-text">Seller email</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="email" name="email" placeholder="Input seller email" className="input input-bordered w-full" required/>
-                    </label>
+                <div className="md:flex mb-8 gap-6">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Rating</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="rating" placeholder="inter a rating" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ">
+                        <label className="label">
+                            <span className="label-text">Available quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="quantity" placeholder="Inter toy quantity" className="input input-bordered w-full" />
+                        </label>
+                    </div>
                 </div>
-            </div>
-            
-            <div className="md:flex mb-8">
-                <div className="form-control md:w-1/2">
-                    <label className="label">
-                        <span className="label-text">Sub Category</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="category" placeholder="Inter Sub Category Toy" className="input input-bordered w-full" />
-                    </label>
+                <div className="md:flex mb-8 ">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Detail description</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="description" placeholder="Detail description" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                  
                 </div>
-                <div className="form-control md:w-1/2 ml-4">
-                    <label className="label">
-                        <span className="label-text">Price</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="price" placeholder="Inter price" className="input input-bordered w-full" />
-                    </label>
-                </div>
-            </div>
-            <div className="md:flex mb-8">
-                <div className="form-control md:w-1/2">
-                    <label className="label">
-                        <span className="label-text">Rating</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="rating" placeholder="Inter rating" className="input input-bordered w-full" />
-                    </label>
-                </div>
-                <div className="form-control md:w-1/2 ml-4">
-                    <label className="label">
-                        <span className="label-text">Available quantity</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="quantity" placeholder="Inter quantity" className="input input-bordered w-full" />
-                    </label>
-                </div>
-            </div>
-          
-            <div className="mb-8">
-                <div className="form-control w-full">
-                    <label className="label">
-                        <span className="label-text">Detail description
-</span>
-                    </label>
-                    <label className="input-group">
-                        <input type="text" name="description" placeholder="Inter detail description" className="input input-bordered w-full" />
-                    </label>
-                </div>
-            </div>
-            <input type="submit" value="Add toy" className="btn w-full text-xl font-bold bg-indigo-500 hover:bg-indigo-300" />
+             
+              
+              <input type="submit" value="add a toy" className="btn w-full font-bold text-xl bg-gray-400 hover:btn-outline" />
 
-        </form>
-    </div>
-    );
+            </form>
+        </div>
+  );
 };
 
 export default AddToyes;
