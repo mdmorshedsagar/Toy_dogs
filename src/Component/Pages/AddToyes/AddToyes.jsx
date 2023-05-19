ba5dc433fc07268641263b92a3d81c80e809f304
mdmorshedsagar <mdmosahidsagar5304257@gmail.com>
2023-05-19 06:10:20 -1200

    allToys-layout-create

--- src/Component/Pages/AddToyes/AddToyes.jsx
+++ src/Component/Pages/AddToyes/AddToyes.jsx
@@ -1,16 +1,14 @@
 import { useContext } from "react";
 import { authContext } from "../../../Provider/AuthProvider";
-import Swal from 'sweetalert2'
-
-
+import Swal from "sweetalert2";
 
 const AddToyes = () => {
-  const { user} = useContext(authContext);
+  const { user } = useContext(authContext);
   const handleAddToy = (event) => {
     event.preventDefault();
-  
+
     const form = event.target;
-  
+
     const name = form.name.value;
     const picture = form.picture.value;
     const seller = form.seller.value;
@@ -19,133 +17,183 @@ const AddToyes = () => {
     const price = form.price.value;
     const rating = form.rating.value;
     const quantity = form.quantity.value;
-      const description = form.description.value;
-    const addToyData={
-      name,picture,seller,email,category,price,rating,quantity,description
-    }
-    console.log(addToyData)
-  
-    fetch('http://localhost:5000/addToy', {
-      method: 'POST',
+    const description = form.description.value;
+    const addToyData = {
+      name,
+      picture,
+      seller,
+      email,
+      category,
+      price,
+      rating,
+      quantity,
+      description,
+    };
+    console.log(addToyData);
+
+    fetch("http://localhost:5000/addToy", {
+      method: "POST",
       headers: {
-          'content-type': 'application/json'
+        "content-type": "application/json",
       },
-      body: JSON.stringify(addToyData)
-  })
-      .then(res => res.json())
-      .then(data => {
-          
-          if(data.insertedId){
-            Swal.fire({
-                title: 'Success!',
-                text: 'Toy Added Successfully',
-                icon: 'success',
-                confirmButtonText: 'Cool'
-              })
-        }
+      body: JSON.stringify(addToyData),
     })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "Toy Added Successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+          form.reset();
+        }
        
-  
+      });
+      
   };
   return (
-    <div className="bg-[#F4F3F0] p-24">
-            <h2 className="text-4xl font-bold text-center py-4">Add a Toy</h2>
-            <form onSubmit={handleAddToy}>
-               
-                <div className="md:flex mb-8 gap-6">
-                    <div className="form-control md:w-1/2">
-                        <label className="label">
-                            <span className="label-text">Picture URL of the toy</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="picture" placeholder="Toy picture" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="name"
-                           
-                            placeholder="Inter toy name" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                </div>
-                <div className="md:flex mb-8 gap-6">
-                    <div className="form-control md:w-1/2">
-                        <label className="label">
-                            <span className="label-text">Seller name</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="seller"
-                             defaultValue={user?.displayName}  placeholder="Seller name" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ">
-                        <label className="label">
-                            <span className="label-text">Seller email</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="email" name="email" 
-                            defaultValue={user?.email}
-                            placeholder="Inter toy name" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                </div>
-                <div className="md:flex mb-8 gap-6">
-                    <div className="form-control md:w-1/2">
-                        <label className="label">
-                            <span className="label-text">Sub-category</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="category" placeholder="Sub-category" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ">
-                        <label className="label">
-                            <span className="label-text">Price</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="price" placeholder="Inter toy price" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                </div>
-                <div className="md:flex mb-8 gap-6">
-                    <div className="form-control md:w-1/2">
-                        <label className="label">
-                            <span className="label-text">Rating</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="rating" placeholder="inter a rating" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ">
-                        <label className="label">
-                            <span className="label-text">Available quantity</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="quantity" placeholder="Inter toy quantity" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                </div>
-                <div className="md:flex mb-8 ">
-                    <div className="form-control w-full">
-                        <label className="label">
-                            <span className="label-text">Detail description</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="description" placeholder="Detail description" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                  
-                </div>
-             
-              
-              <input type="submit" value="add a toy" className="btn w-full font-bold text-xl bg-gray-400 hover:btn-outline" />
-
-            </form>
+    <div className="bg-[#F4F3F0] p-16">
+      <h2 className="text-4xl font-bold text-center pb-4">Add a Toy</h2>
+      <form onSubmit={handleAddToy}>
+        <div className="md:flex mb-8 gap-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Picture URL of the toy</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="picture"
+                placeholder="Toy picture"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 ">
+            <label className="label">
+              <span className="label-text">Toy Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="Inter toy name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
         </div>
+        <div className="md:flex mb-8 gap-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Seller name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="seller"
+                defaultValue={user?.displayName}
+                placeholder="Seller name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 ">
+            <label className="label">
+              <span className="label-text">Seller email</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="email"
+                name="email"
+                defaultValue={user?.email}
+                placeholder="Inter toy name"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="md:flex mb-8 gap-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Sub-category</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="category"
+                placeholder="Sub-category"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 ">
+            <label className="label">
+              <span className="label-text">Price</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="price"
+                placeholder="Inter toy price"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="md:flex mb-8 gap-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Rating</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="rating"
+                placeholder="inter a rating"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 ">
+            <label className="label">
+              <span className="label-text">Available quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Inter toy quantity"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="md:flex mb-8 ">
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Detail description</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="description"
+                placeholder="Detail description"
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+        </div>
+
+        <input
+          type="submit"
+          value="add a toy"
+          className="btn w-full font-bold text-xl bg-gray-400 hover:btn-outline"
+        />
+      </form>
+    </div>
   );
 };
 
--- src/Component/Pages/AllToyes/AllToyes.jsx
+++ src/Component/Pages/AllToyes/AllToyes.jsx
@@ -2,9 +2,31 @@
 
 const AllToyes = () => {
     return (
-        <div>
-           sdfsf 
-        </div>
+        <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          {/* head */}
+          <thead >
+            <tr >
+              <th>seller</th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+           
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Cy Gandertons</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+          
+          </tbody>
+        </table>
+      </div>
     );
 };
 
