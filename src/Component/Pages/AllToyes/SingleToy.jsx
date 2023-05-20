6e602e6d219ac387b78984a5e4e11216c32b4eac
mdmorshedsagar <mdmosahidsagar5304257@gmail.com>
2023-05-19 13:42:32 -1200

    vercel-server-connect

--- src/Component/Pages/AllToyes/SingleToy.jsx
+++ src/Component/Pages/AllToyes/SingleToy.jsx
@@ -1,6 +1,6 @@
 import {
     
-    useLoaderData
+    useLoaderData,Link
   } from "react-router-dom";
 
 
@@ -10,21 +10,21 @@ const SingleToy = () => {
 //    console.log(name,picture,seller,email,category,price,rating,quantity,description)
     return (
         <div className="card lg:card-side bg-slate-100 shadow-xl m-12 p-6 ">
-  <figure className="w-[50%] h-full"><img src={picture} alt="Album" className="w-[full] h-full"/></figure>
-  <div className="card-body bg-white">
-    <h2 className="card-title">Toy name: {name}</h2>
-    <h2 className="text-xl font-bold">Seller name: {seller}</h2>
+  <figure className="w-[100%] h"><img src={picture} alt="Album" className="w-full h-full"/></figure>
+  <div className="card-body bg-white w-[100%]">
+    <h2 className="card-title text-2xl"> <span className="font-bold">Toy name:</span> {name}</h2>
+    <h2 className="text-xl font-bold"> <span className="font-bold text-xl">Seller name:</span> {seller}</h2>
 
-    <p>email: {email}</p>
-    <p>Sub category: {category}</p>
-    <p>description: {description}</p>
-    <p>Price: {price}</p>
-    <p>quantity: {quantity}</p>
+    <p><span className="font-bold">email:</span> {email}</p>
+    <p><span className="font-bold">Sub category:</span> {category}</p>
+    <p><span className="font-bold">description:</span> {description}</p>
+    <p><span className="font-bold">Price:</span> ${price}</p>
+    <p><span className="font-bold">quantity:</span> {quantity}</p>
     
 
     <div className="card-actions flex justify-between items-center">
         <div>sdfasf {rating}</div>
-      <button className="btn btn-primary">Listen</button>
+      <Link to='/' className="btn bg-black hover:bg-white hover:text-black hover:rounded-xl">Back Home</Link>
     </div>
   </div>
 </div>
--- src/Component/Pages/HomeComponent/ShopCategory.jsx
+++ src/Component/Pages/HomeComponent/ShopCategory.jsx
@@ -8,7 +8,7 @@ const ShopCategory = () => {
   const [category, setCategory] = useState("Teddy");
   
   useEffect(() => {
-    fetch(`http://localhost:5000/${category}`)
+    fetch(`https://dog-toys-server.vercel.app/${category}`)
       .then((res) => res.json())
       .then((result) => {
         setTabs(result);
